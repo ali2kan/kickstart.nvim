6e1a3c578aaad88a887dcec8b6d954f867e9ec43
ali2kan <60204416+ali2kan@users.noreply.github.com>
2024-04-20 14:29:26 -0700

    Update lazy.nvim setup and add telescope plugin for todo searching

--- lua/ali2kan/lazy.lua
+++ lua/ali2kan/lazy.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("ali2kan.plugins")
+require("lazy").setup("ali2kan.plugins", {
+  checker = {
+    enables = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
