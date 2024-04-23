1748544f33bf44602d99f1787fffc805f348b959
ali2kan <60204416+ali2kan@users.noreply.github.com>
2024-04-22 20:03:42 -0700

    Colorizer

--- lua/ali2kan/plugins/indent-blankline.lua
+++ lua/ali2kan/plugins/indent-blankline.lua
@@ -2,7 +2,15 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   event = { "BufReadPre", "BufNewFile" },
   main = "ibl",
-  opts = {
-    indent = { char = "┊" },
+  -- opts = {
+  -- indent = { char = "┊" },
+  -- },
+  -- "lukas-reineke/indent-blankline.nvim",
+  opts = function(_, opts)
+    -- Other blankline configuration here
+    return require("indent-rainbowline").make_opts(opts)
+  end,
+  dependencies = {
+    "TheGLander/indent-rainbowline.nvim",
   },
 }
