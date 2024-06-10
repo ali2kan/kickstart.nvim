ea0690a41164fd3bc1db6fd677ed8b516a8ea684
ali2kan <60204416+ali2kan@users.noreply.github.com>
2024-06-10 08:56:10 -0700

    comented out move.lua

--- lua/ali2kan/plugins/move.lua
+++ lua/ali2kan/plugins/move.lua
@@ -19,6 +19,25 @@ return {
   --   })
   --   local opts = { noremap = true, silent = true }
   --
+  opts = {
+    noremap = true,
+    silent = true,
+  },
+  -- Normal-mode commands
+  keys = {
+    vim.keymap.set("n", "<A-j>", ":MoveLine(1)<CR>"),
+    vim.keymap.set("n", "<A-k>", ":MoveLine(-1)<CR>"),
+    vim.keymap.set("n", "<A-h>", "<cmd>:MoveHChar(-1)<CR>"),
+    vim.keymap.set("n", "<A-l>", "<cmd>:MoveHChar(1)<CR>"),
+    vim.keymap.set("n", "<leader>wf", "<cmd>:MoveWord(1)<CR>"),
+    vim.keymap.set("n", "<leader>wb", "<cmd>:MoveWord(-1)<CR>"),
+
+    -- Visual-mode commands
+    vim.keymap.set("v", "<A-j>", "<cmd>:MoveBlock(1)<CR>"),
+    vim.keymap.set("v", "<A-k>", "<cmd>:MoveBlock(-1)<CR>"),
+    vim.keymap.set("v", "<A-h>", "<cmd>:MoveHBlock(-1)<CR>"),
+    vim.keymap.set("v", "<A-l>", "<cmd>:MoveHBlock(1)<CR>"),
+  },
   --   -- Normal-mode commands
   --   vim.keymap.set("n", "<A-j>", ":MoveLine(1)<CR>", opts)
   --   vim.keymap.set("n", "<A-k>", ":MoveLine(-1)<CR>", opts)
